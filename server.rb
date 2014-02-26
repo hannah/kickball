8254a1e9eb404a68f997e2ff64d8d5d5bbd38c65
cwonrails <cmwatson@gmail.com>
2014-02-26 16:10:35 -0500

    populated an array of hashes for players

--- server.rb
+++ server.rb
@@ -1,4 +1,15 @@
 require 'sinatra'
+require 'csv'
+
+roster = []
+
+CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
+  roster << row.to_hash
+end
+
+
+puts roster
+
 
 get '/' do
   ##@tasks = ['pay bills', 'buy milk', 'learn Ruby']
