cd56329c861176ca776d800e4319d8f1e44f320b
cwonrails <cmwatson@gmail.com>
2014-02-26 17:28:17 -0500

    fixed server.rb syntax errors to get individual teams

--- server.rb
+++ server.rb
@@ -1,18 +1,26 @@
-require 'sinatra'
+#require 'sinatra'
 require 'csv'
 
-roster = []
+@roster = []
 
 CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
-  roster << row.to_hash
+  @roster << row.to_hash
 end
 
-
-puts roster
-
-
-get '/' do
-  ##@tasks = ['pay bills', 'buy milk', 'learn Ruby']
-  ##above was for tasks list - likely should be reflected in index.erb
-  erb :index
+def simpson_players
+  @roster.select {|player| player["team"] == "Simpson Slammer"}
 end
+
+def jetson_players
+  @roster.select {|player| player["team"] == "Jetson Jets"}
+end
+
+def flintstone_players
+  @roster.select {|player| player["team"] == "Flintstone Fire"}
+end
+
+def griffin_players
+  @roster.select {|player| player["team"] == "Griffin Goats"}
+end
+
+puts griffin_players
