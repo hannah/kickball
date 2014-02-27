287fe3f27e16b89ea564ac0849730c5bcb47e4ad
cwonrails <cmwatson@gmail.com>
2014-02-27 12:00:10 -0500

    Populated an array with team specific to individual webpage

--- server.rb
+++ server.rb
@@ -1,24 +1,49 @@
 require 'sinatra'
 require 'csv'
 
-@roster = []
-
-CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
-  @roster << row.to_hash
+get '/Simpson_Slammers' do
+  @simpsons = []
+  CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
+    if row["team"] == "Simpson Slammers"
+      @simpsons << row.to_hash
+    end
+  end
+  @simpsons
 end
 
-def simpson_players
-  @roster.select {|player| player["team"] == "Simpson Slammer"}
-end
+# get '/Jetson_Jets' do
+#   @jetsons = []
+#   CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
+#     @roster << row.to_hash
+#   end
+# end
 
-def jetson_players
-  @roster.select {|player| player["team"] == "Jetson Jets"}
-end
+# get '/Flinstone_Fire' do
+#   @flintstones = []
+#   CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
+#     @roster << row.to_hash
+#   end
+# end
 
-def flintstone_players
-  @roster.select {|player| player["team"] == "Flintstone Fire"}
-end
+# get '/Griffin_Goats' do
+#   @griffin = []
+#   CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
+#     @roster << row.to_hash
+#   end
+# end
 
-def griffin_players
-  @roster.select {|player| player["team"] == "Griffin Goats"}
-end
+# def simpson_players
+#   @roster.select {|player| player["team"] == "Simpson Slammers"}
+# end
+
+# def jetson_players
+#   @roster.select {|player| player["team"] == "Jetson Jets"}
+# end
+
+# def flintstone_players
+#   @roster.select {|player| player["team"] == "Flintstone Fire"}
+# end
+
+# def griffin_players
+#   @roster.select {|player| player["team"] == "Griffin Goats"}
+# end
