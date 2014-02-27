d8ec89fe38cbc158441e3a7c341edb290dece364
cwonrails <cmwatson@gmail.com>
2014-02-27 15:48:40 -0500

    Rebuilt index.rb to allow for properly formatted dynamic data population

--- server.rb
+++ server.rb
@@ -1,49 +1,18 @@
 require 'sinatra'
 require 'csv'
+require 'pry'
 
-get '/Simpson_Slammers' do
-  @simpsons = []
+
+
+get '/:team_name' do
+  @players = []
   CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
-    if row["team"] == "Simpson Slammers"
-      @simpsons << row.to_hash
+    if row["team"] == params[:team_name]
+      @players << row.to_hash
     end
   end
-  @simpsons
+  erb :index
 end
 
-# get '/Jetson_Jets' do
-#   @jetsons = []
-#   CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
-#     @roster << row.to_hash
-#   end
-# end
-
-# get '/Flinstone_Fire' do
-#   @flintstones = []
-#   CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
-#     @roster << row.to_hash
-#   end
-# end
-
-# get '/Griffin_Goats' do
-#   @griffin = []
-#   CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
-#     @roster << row.to_hash
-#   end
-# end
-
-# def simpson_players
-#   @roster.select {|player| player["team"] == "Simpson Slammers"}
-# end
-
-# def jetson_players
-#   @roster.select {|player| player["team"] == "Jetson Jets"}
-# end
-
-# def flintstone_players
-#   @roster.select {|player| player["team"] == "Flintstone Fire"}
-# end
-
-# def griffin_players
-#   @roster.select {|player| player["team"] == "Griffin Goats"}
-# end
+set :public_folder, File.dirname(__FILE__) + '/public'
+set :views_folder, File.dirname(__FILE__) + '/views'
