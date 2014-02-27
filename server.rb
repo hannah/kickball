bcf2ca0e61e3fca104c9cc4b88cd254c5d5afc0e
cwonrails <cmwatson@gmail.com>
2014-02-27 16:24:28 -0500

    Updated server.rb and index.erb to ensure team name was dynamically populated

--- server.rb
+++ server.rb
@@ -2,10 +2,9 @@ require 'sinatra'
 require 'csv'
 require 'pry'
 
-
-
 get '/:team_name' do
   @players = []
+  @team_name = params[:team_name]
   CSV.foreach("lackp_starting_rosters.csv", headers: true) do |row|
     if row["team"] == params[:team_name]
       @players << row.to_hash
